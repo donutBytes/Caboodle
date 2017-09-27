0de434e30391f0e684a20a591e316f3cb9be5605
Rbilemjian <rbilemjian@gmail.com>
2017-09-27 10:00:47 -0700

    Changing titles of page views to replace dashboard text
    This is a really underwhelming commit lmao

--- resources/views/favorites.blade.php
+++ resources/views/favorites.blade.php
@@ -5,10 +5,9 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Your Favorites</div>
 
                 <div class="panel-body">
-                        Your favorites
                 </div>
             </div>
         </div>
