c14aaf04b69f8a3e4c331997b43e64ac00769f27
Rbilemjian <rbilemjian@gmail.com>
2017-09-30 23:17:24 -0700

    Item view somehow didn't make it through the last push
    whoops

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -5,9 +5,11 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Some Item</div>
-
+                <div class="panel-heading"><b>Item: {{$item->name}}</b></div>
                 <div class="panel-body">
+                    <li><b>Description: </b>{{$item->description}}</li>
+                    <li><b>Condition: </b>{{$item->condition}}</li>
+                    <li><b>Cost: </b>${{$item->cost}}</li>
                 </div>
             </div>
         </div>
