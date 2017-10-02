7a7bcff497bd5e191bde052cf0634e6337289abe
donutBytes <alexander.bickham.885@gmail.com>
2017-10-01 22:57:17 -0700

    Pulling again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,11 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Your Kits</div>
-                  <div class="panel-body">
-                    @foreach($kits as $kit)
-                      <li><a href="/kit/{{$kit->kit_id}}">{{$kit->kit_name}}</li></a>
-                    @endforeach
-                  </div>
+                    <div class="panel-body">
+                        @foreach($kits as $kit)
+                            <a href="/kit/{{$kit->kit_id}}"><li>{{$kit->kit_name}}</li></a>
+                        @endforeach
+                    </div>
             </div>
         </div>
     </div>
